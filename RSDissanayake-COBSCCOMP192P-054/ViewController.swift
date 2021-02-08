01aac105bd8c52adc04b6c73b21d88b0eee30444
RavindraSandun <sandun.hotmail@gmail.com>
2021-02-08 21:43:10 +0530

    first commit

---
+++ RSDissanayake-COBSCCOMP192P-054/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RSDissanayake-COBSCCOMP192P-054
+//
+//  Created by Ravindra Sandun Dissanayake on 2/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
